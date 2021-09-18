d7134ca3e4bfa2464461d912a4e7ca4e6450866b
Lina <alexis.lina@sewan.fr>
2021-09-18 08:21:10 +0200

    feat: Add infinite loop with reminder, delete use because of duplicate

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,4 @@
-use super::method::Method;
 pub struct Request {
     path: String,
     query_string: Option<String>,
-    method: Method,
 }
--- src/server.rs
+++ src/server.rs
@@ -10,6 +10,24 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
-        let listenenr = TcpListener::bind(&self.addr);
+        let listenenr = TcpListener::bind(&self.addr).unwrap(); //unwrap handle error if addr already use and kill program
+
+        //Keyword for infinite loop, we can use while but rust include this keyword for that
+        // we can add label to our loop, ex : 'outer: loop
+        // this allow us in some cases to break for example our 'outer' loop in a inner loop
+        // Example:
+        // 'outer: loop{
+        //    'inner: loop {
+        //        break 'outer;
+        //    }
+        // }
+         loop {
+
+            let result = listenenr.accept(); // accept return two value
+
+            if result.is_err() {
+                continue;
+            }
+        }
     }
 }
