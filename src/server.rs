2c52ae8fbd263258c118684d84cdedcadc3d13eb
Lina <alexis.lina@sewan.fr>
2021-09-18 10:09:42 +0200

    feat: Able to read from socket and launch server work fine

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::io::{Read};
+use std::str;
 
 pub struct Server {
     addr: String,
@@ -12,7 +14,7 @@ impl Server {
         println!("Listening on {}", self.addr);
         let listenenr = TcpListener::bind(&self.addr).unwrap(); //unwrap handle error if addr already use and kill program
 
-        // Keyword for infinite loop, we can use while but rust include this keyword for that
+        // loop is a keyword for infinite loop, we can use while but rust include this keyword for that
         // we can add label to our loop, ex : 'outer: loop
         // this allow us in some cases to break for example our 'outer' loop in a inner loop
         // Example:
@@ -27,8 +29,16 @@ impl Server {
              match listenenr.accept() {
                  // Here we want only the TCPStream returned by accept method
                  // we don't care for now about Sockaddr
-                 Ok((stream, _)) => {
-                     println!("Incoming connection accept");
+                 Ok((mut stream, _)) => {
+                     let mut buf = [0; 1024];
+                     match stream.read(&mut buf) {
+                         Ok(_) => {
+                             println!("Received request: {}", String::from_utf8_lossy(&buf));
+                         }
+                         Err(e) => {
+                             print!("Failed to read from connection {}", e);
+                         }
+                     }
                  },
                  // We handle error if something wrong
                  Err(e) => {
